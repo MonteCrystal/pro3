14a711b9c6c13a4b073c3c755f5ebab765e43fec
MonteCrystal <58733371+MonteCrystal@users.noreply.github.com>
2021-12-17 17:19:43 +0800

    hello

--- src/components/DisplayMenu.vue
+++ src/components/DisplayMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <p>目录</p>
-    <el-menu router=true>
+    <el-menu :router=true>
       <el-menu-item index='/main'>报告</el-menu-item>
       <el-menu-item index='/main/datalist' @click="openData">数据</el-menu-item>
       <el-menu-item index='/main/history' disabled>历史记录</el-menu-item>
